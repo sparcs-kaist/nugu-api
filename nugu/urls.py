0bedb48f8f08398877b017d50f3387b86863cbd7
Luan Sangguk Lee <sangguk258@gmail.com>
2017-07-18 12:38:50 +0900

    chaned default permission class to IsAuthenticated

--- nugu/urls.py
+++ nugu/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url, include
+from django.contrib import admin
 from rest_framework.urlpatterns import format_suffix_patterns
 from apps.api import views
 
 urlpatterns = [
     url(r'^users/$', views.user_list),
     url(r'^users/(?P<pk>[A-Za-z0-9]+)$', views.user_detail),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^admin/', admin.site.urls)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
