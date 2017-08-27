9c3793751bcc83287198ad83b99a99204b25dc36
Luan Sangguk Lee <sangguk258@gmail.com>
2017-08-27 22:52:21 +0900

    Delete useless comments, unused methods

--- apps/api/models.py
+++ apps/api/models.py
@@ -7,8 +7,6 @@ from sqlalchemy import create_engine
 '''
 from django.db import models
 
-# from .settings import DB_URL
-
 
 NUGU_FIELDS = [
     {'id': 'name', 'name': '이름', 'hint': '뀨냥이'},
@@ -45,7 +43,7 @@ class User(models.Model):
 
     id = models.CharField(max_length=30, primary_key=True)
     name = models.CharField(max_length=255, null=True)
-    is_developer = models.BooleanField(default=True)
+    is_developer = models.BooleanField(default=False)
     is_designer = models.BooleanField(default=False)
     is_undergraduate = models.BooleanField(default=True)
     ent_year = models.CharField(max_length=255, null=True)
@@ -66,81 +64,3 @@ class User(models.Model):
     blog = models.CharField(max_length=255, null=True)
     created_on = models.DateTimeField(default=timezone.now)
     updated_on = models.DateTimeField(auto_now=True)
-
-    @staticmethod
-    def _gen_json(value_func):
-        result = ['{', ]
-        for i in NUGU_FIELDS:
-            if i.get('readonly', False):
-                continue
-            result.append('    "%s": "%s",' % (i['id'], value_func(i)))
-        result[-1] = result[-1][:-1]
-        result.append('}')
-        return '\n'.join(result)
-
-    @staticmethod
-    def default_json():
-        return User._gen_json(lambda i: i.get('hint', ''))
-
-    def to_json(self):
-        return self._gen_json(lambda i: getattr(self, i['id']) or "")
-
-    def __str__(self):
-        return u'%s (%s, %s)' % (self.id, self.name, self.ent_year)
-
-
-'''
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = 'user'
-
-    id = Column(String(30), primary_key=True)
-    name = Column(String(255), nullable=True)
-    ent_year = Column(String(255), nullable=True)
-    org = Column(String(255), nullable=True)
-    email = Column(String(255), nullable=True)
-    phone = Column(String(255), nullable=True)
-    birth = Column(String(255), nullable=True)
-    dorm = Column(String(255), nullable=True)
-    lab = Column(String(255), nullable=True)
-    home_add = Column(String(255), nullable=True)
-    facebook_id = Column(String(255), nullable=True)
-    twitter_id = Column(String(255), nullable=True)
-    github_id = Column(String(255), nullable=True)
-    battlenet_id = Column(String(255), nullable=True)
-    website = Column(String(255), nullable=True)
-    blog = Column(String(255), nullable=True)
-    created_on = Column(DateTime, default=datetime.utcnow)
-    updated_on = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-
-    @staticmethod
-    def _gen_json(value_func):
-        result = ['{', ]
-        for i in NUGU_FIELDS:
-            if i.get('readonly', False):
-                continue
-            result.append('    "%s": "%s",' % (i['id'], value_func(i)))
-        result[-1] = result[-1][:-1]
-        result.append('}')
-        return '\n'.join(result)
-
-    @staticmethod
-    def default_json():
-        return User._gen_json(lambda i: i.get('hint', ''))
-
-    def to_json(self):
-        return self._gen_json(lambda i: getattr(self, i['id']) or "")
-
-    def __str__(self):
-        return u'%s (%s, %s)' % (self.id, self.name, self.ent_year)
-
-def create_session():
-    engine = create_engine(DB_URL, pool_recycle=100)
-    if not engine.dialect.has_table(engine, User.__tablename__):
-        Base.metadata.create_all(engine)
-    Base.metadata.bind = engine
-    DBSession = sessionmaker(bind=engine)
-    return DBSession()
-
-'''
